c461eb224bb25767cf46529d2a3a02089695bced
abdulrahimans19 <117072532+abdulrahimans19@users.noreply.github.com>
2023-09-29 18:04:44 +0530

    initial commits

---
+++ routes/profile.js
@@ -0,0 +1,8 @@
+import express from "express";
+import { getProfile } from "../controllers/profile.js";
+import authentication from "../middileware/auth.js";
+
+const router = express.Router();
+
+router.get("/", authentication, getProfile);
+export default router;
